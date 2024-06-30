24ed0ffd975292f70dd9829dd80f615c00eaae53
Alex Derevianko <sasha.derevyanko.2018.mail.ru>
2024-07-01 01:06:46 +0300

    ban

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { useState } from 'react'
+import { Header } from './header/Header'
+import './App.sass'
+import { chengeTheme } from './chengeTheme';
 
 function App() {
-  
+  const [theme, setTheme] = useState('white');
+  const [themeFon, setThemeFon] = useState('black');
+
+  console.log(setTheme);
+  console.log('ban');
   return (
     <>
-      <button>Тема</button>
-      <button></button>
+    <div className='container'>
+        <button onClick={() => chengeTheme(setTheme())}>Тема</button>
+        <div className='main' style={{backgroundColor: themeFon}}>
+          <Header theme={theme}/>
+          <Header theme={theme}/>
+          <Header theme={theme}/>
+          <Header theme={theme}/>
+        </div>
+      </div>
     </>
   )
 
